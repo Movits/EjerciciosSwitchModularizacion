0171d2d9a2370a6df3af95c023a71284ae912292
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-21 15:12:58 -0300

    agregue funciones de calc descuento y regalo

--- Ej1/script.js
+++ Ej1/script.js
@@ -1,10 +1,11 @@
 const pPrecioTotal = document.querySelector("#precioTotal");
-const PRECIO_PROD_1 = 500;
-const PRECIO_PROD_2 = 300;
 const selectCant = document.querySelector("select");
 const olListaCarrito = document.querySelector("#listaCarrito");
 const PROD_1 = "Pan";
 const PROD_2 = "Medialuna";
+const PRECIO_PROD_1 = 500;
+const PRECIO_PROD_2 = 300;
+
 
 let precioTotal = 0;
 let tipoProducto = document.querySelector("#tipo-producto");
@@ -13,6 +14,23 @@ let imgProducto = document.querySelector("#img-producto");
 
 let nombreProductoActual = PROD_1;
 let precioProductoActual = PRECIO_PROD_1;
+
+function calcularDescuento(cantidad, producto) {
+    let descuento = 0;
+    if (producto === PROD_1) {
+        if (cantidad === 3) descuento = 0.10;
+        else if (cantidad === 7) descuento = 0.25;
+    } else if (producto === PROD_2) {
+        if (cantidad === 1) descuento = 0.15;
+        else if (cantidad === 7) descuento = 0.30;
+    }
+    return descuento;
+}
+
+function calcularRegalo(cantidad, producto) {
+    if (producto === PROD_2 && cantidad === 7) return 1;
+    return 0;
+}
 
 function agregarProducto() {
     precioTotal = (precioProductoActual * selectCant.value) + precioTotal;
@@ -26,7 +44,7 @@ function siguienteProducto() {
     
 
     tipoProducto.innerHTML = PROD_2;
-    precioProducto.innerHTML = "Precio: " + PRECIO_PROD_2;
+    precioProducto.innerHTML = "Precio: " + PRECIO_PROD_2 + "$";
     imgProducto.src = "https://www.abc.com.py/resizer/XRvOD1fw0nVKkmGJY6JbE21yZkE=/fit-in/770x495/smart/filters:format(webp)/cloudfront-us-east-1.images.arcpublishing.com/abccolor/PVBO4DFYMZH2NHZTQPE7H3P7HE.jpg";
 
     nombreProductoActual = PROD_2;
