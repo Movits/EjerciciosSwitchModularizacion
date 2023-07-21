1024264328bb6d1a8171fea97c1fdf146f43ec04
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-21 02:14:37 -0300

    agregue una lista de productos en la funcion de agregarProductos

--- Ej1/script.js
+++ Ej1/script.js
@@ -1,10 +1,14 @@
 const pPrecioTotal = document.querySelector("#precioTotal");
 const PRECIO_PROD_1 = 500;
 const selectCant = document.querySelector("select");
+const olListaCarrito = document.querySelector("#listaCarrito");
 
 let precioTotal = 0;
 
 function agregarProducto() {
     precioTotal = (PRECIO_PROD_1 * selectCant.value) + precioTotal;
     pPrecioTotal.innerHTML = "Total: $" + precioTotal;
+    olListaCarrito.innerHTML += `
+    <li>Pan</li>
+    `;
 }
