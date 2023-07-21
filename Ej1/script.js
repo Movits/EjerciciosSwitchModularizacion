6a4fd18ef617b42f4ed13d45b23f8438fe7f2393
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-21 02:09:57 -0300

    arregle precio total

--- Ej1/script.js
+++ Ej1/script.js
@@ -2,7 +2,9 @@ const pPrecioTotal = document.querySelector("#precioTotal");
 const PRECIO_PROD_1 = 500;
 const selectCant = document.querySelector("select");
 
+let precioTotal = 0;
 
 function agregarProducto() {
-    pPrecioTotal.innerHTML = "Total: $" + selectCant.value * PRECIO_PROD_1;
+    precioTotal = (PRECIO_PROD_1 * selectCant.value) + precioTotal;
+    pPrecioTotal.innerHTML = "Total: $" + precioTotal;
 }
