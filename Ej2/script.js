5a3cfd7428ea522d204ddaba7c885f4cb7e3ea71
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-22 22:33:05 -0300

    agregue pagina de gracias

--- Ej2/script.js
+++ Ej2/script.js
@@ -2,8 +2,9 @@ const fieldset = document.querySelector("#fieldset1");
 const pInfoSelec = document.querySelector("#pInfo");
 const IMG_2 = "https://media.admagazine.com/photos/63b8643347e50dfe1e2f4997/master/w_3000,h_2000,c_limit/cama-de-hotel-cabecero.jpg";
 const IMG_3 = "https://earthsky.org/upl/2014/04/532px-Cranmer_Sundial.jpg";
-const IMG_4 = "https://www.guiarepsol.com/content/dam/repsol-guia/contenidos-imagenes/comer/cultura-gastronomica/el-desayuno-de-hotel-mas-alla-del-bufe/gr-cms-media-featured_images-none-c58d3bd2-57c8-4094-941e-f29f1599a756-1.jpg";
-const IMG_5 = "https://media.tenor.com/r5a_QxNpB6UAAAAC/gracias-thanks.gif";
+const IMG_4 = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcShhju8PdiVsXHC7QapvYp1Lhx4K4FBe5NntjAf58gapjlcCevX-yw1p8FPifnfuD3jt_k&usqp=CAU";
+const IMG_5 = "https://www.guiarepsol.com/content/dam/repsol-guia/contenidos-imagenes/comer/cultura-gastronomica/el-desayuno-de-hotel-mas-alla-del-bufe/gr-cms-media-featured_images-none-c58d3bd2-57c8-4094-941e-f29f1599a756-1.jpg";
+const IMG_6 = "https://media.tenor.com/r5a_QxNpB6UAAAAC/gracias-thanks.gif";
 
 let reserva = {
     camas: null,
@@ -53,29 +54,9 @@ function grabarDias() {
     pInfoSelec.innerHTML = pInfoSelec.innerHTML + ", " + reserva.dias + " dia/s";
   }
 
-function pagDias() {
-    fieldset.innerHTML = `
-        <img src="${IMG_3}">
-        <fieldset id="fieldset2">
-            <p>Cantidad de Dias:</p>
-            <select name="" id="diasOpc">
-                <option value="1">1</option>
-                <option value="3">3</option>
-                <option value="7">7</option>
-            </select>
-        </fieldset>
-        <button onclick="grabarDias(); pagIngreso();">Seguinte</button>
-    `;
-}
-
-function grabarDias() {
-    reserva.dias = document.querySelector("#diasOpc").value;
-    pInfoSelec.innerHTML = pInfoSelec.innerHTML + ", " + reserva.dias + " dia/s";
-  }
-
 function pagIngreso() {
     fieldset.innerHTML = `
-        <img src="${IMG_3}">
+        <img src="${IMG_4}">
         <fieldset id="fieldset2">
             <p>Dia de Ingreso:</p>
             <select name="" id="diasIngresoOpc">
@@ -90,12 +71,12 @@ function pagIngreso() {
 
 function grabarIngreso() {
     reserva.ingreso = document.querySelector("#diasIngresoOpc").value;
-    pInfoSelec.innerHTML = pInfoSelec.innerHTML + ", Ingresa en " + reserva.ingreso;
+    pInfoSelec.innerHTML = pInfoSelec.innerHTML + ", Ingresa " + reserva.ingreso;
   }
 
 function pagServicios() {
     fieldset.innerHTML = `
-        <img src="${IMG_4}">
+        <img src="${IMG_5}">
         <fieldset id="fieldset2">
             <p>Servicios Extra:</p>
             <select name="" id="serviciosOpc">
@@ -104,11 +85,17 @@ function pagServicios() {
                 <option value="Desayuno, almuerzo y cena">Desayuno, almuerzo y cena</option>
             </select>
         </fieldset>
-        <button onclick="grabarServicios(); pagGracias();">Seguinte</button>
+        <button onclick="grabarServicios(); pagGracias();">Finalizar Reserva</button>
     `;
 }
 
 function grabarServicios() {
     reserva.servicios = document.querySelector("#serviciosOpc").value;
-    pInfoSelec.innerHTML = pInfoSelec.innerHTML + " y " + reserva.servicios + " de servicios.";
+    pInfoSelec.innerHTML = pInfoSelec.innerHTML + ", " + reserva.servicios + " de servicio/s.";
   }
+
+function pagGracias() {
+    fieldset.innerHTML = `
+        <img src="${IMG_6}">
+    `;
+}
