90b11e83cf10e603435f8b028999b46fbc8fef5b
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-22 21:44:40 -0300

    agregue la pagina de dias

--- Ej2/script.js
+++ Ej2/script.js
@@ -1,6 +1,7 @@
 const fieldset = document.querySelector("#fieldset1");
-const IMG_2 = "https://media.admagazine.com/photos/63b8643347e50dfe1e2f4997/master/w_3000,h_2000,c_limit/cama-de-hotel-cabecero.jpg";
 const pInfoSelec = document.querySelector("#pInfo");
+const IMG_2 = "https://media.admagazine.com/photos/63b8643347e50dfe1e2f4997/master/w_3000,h_2000,c_limit/cama-de-hotel-cabecero.jpg";
+const IMG_3 = "https://img.freepik.com/vector-premium/reloj-vector-icono-fondo-blanco-ilustracion-vectorial_736051-174.jpg";
 
 let reservation = {
     camas: null,
@@ -28,4 +29,24 @@ function pagCama() {
 function grabarCama() {
     reservation.camas = document.querySelector("#camasOpc").value;
     pInfoSelec.innerHTML = "Reserva: " + reservation.camas + " cama/s";
+  }
+
+function pagDias() {
+    fieldset.innerHTML = `
+        <img src="${IMG_3}">
+        <fieldset id="fieldset2">
+            <p>Cantidad de Dias:</p>
+            <select name="" id="diasOpc">
+                <option value="1">1</option>
+                <option value="3">3</option>
+                <option value="7">7</option>
+            </select>
+        </fieldset>
+        <button onclick="grabarDias(); pagIngreso();">Seguinte</button>
+    `;
+}
+
+function grabarDias() {
+    reservation.dias = document.querySelector("#diasOpc").value;
+    pInfoSelec.innerHTML = pInfoSelec.innerHTML + ", " + reservation.dias + " dia/s";
   }
