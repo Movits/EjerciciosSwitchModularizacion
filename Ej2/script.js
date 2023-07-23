e3f3a9f917d9cb7bbab4e2feb50203fd665a738b
Roberto Alagemovits <robertomovits@gmail.com>
2023-07-22 21:31:14 -0300

    arregle las funciones para cant de camas y cambiar pagina

--- Ej2/script.js
+++ Ej2/script.js
@@ -2,16 +2,30 @@ const fieldset = document.querySelector("#fieldset1");
 const IMG_2 = "https://media.admagazine.com/photos/63b8643347e50dfe1e2f4997/master/w_3000,h_2000,c_limit/cama-de-hotel-cabecero.jpg";
 const pInfoSelec = document.querySelector("#pInfo");
 
-function proximaPagina() {
+let reservation = {
+    camas: null,
+    dias: null,
+    llegada: null,
+    servicios: null
+  };
+
+function pagCama() {
     fieldset.innerHTML = `
         <img src="${IMG_2}">
         <fieldset id="fieldset2">
             <p>Cantidad de Camas:</p>
-            <select name="" id="">
+            <select name="" id="camasOpc">
                 <option value="1">1</option>
                 <option value="2">2</option>
             </select>
         </fieldset>
-        <button onclick="proximaPagina()">Seguinte</button>
+        <button onclick="grabarCama(); pagDias();">Seguinte</button>
     `;
+
+    pInfoSelec.innerHTML = ``;
 }
+
+function grabarCama() {
+    reservation.camas = document.querySelector("#camasOpc").value;
+    pInfoSelec.innerHTML = "Reserva: " + reservation.camas + " cama/s";
+  }
